519b48933642e7197ac849228a7ec7058f9866a5
Abhishek Kumar <67472833+AbhishekKumar2233@users.noreply.github.com>
2022-01-15 22:52:06 +0530

    nested components

---
+++ src/Components/Avatar.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Avatar(props) {
+  return <img src={props.img} alt="avtar_img" />;
+}
--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,11 +1,12 @@
 import React from "react";
+import Avatar from "./Avatar";
 import "./styles.css";
 
 export default function Card(props) {
   return (
     <div className="card">
       <h2>{props.name}</h2>
-      <img src={props.img} alt="avtar_img" />
+      <Avatar img={props.img} />
       <p>{props.contact}</p>
       <p>{props.email}</p>
     </div>
